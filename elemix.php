d6a6d4a7efcb614639a151a3c3eb74ce29c9626d
Ivan Neil Tapel <tapsivanneil@gmail.com>
2024-02-18 16:16:36 +0800

    base input values

---
+++ elemix.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form method="post">
+        <input type="text">
+        <input type="number">
+        <div>+</div>
+        <input type="text">
+        <input type="number">
+        <input type="submit" value="Submit">
+    </form>
+
+    <div>image</div>
+
+
+</body>
+</html>
