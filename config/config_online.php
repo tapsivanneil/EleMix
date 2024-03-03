c102a90bc5eb7c531e3fd32c34d7d6cecbdcaa44
Ivan Neil Tapel <tapsivanneil@gmail.com>
2024-03-03 10:27:12 +0800

    add config online in the files

---
+++ config/config_online.php
@@ -0,0 +1,9 @@
+<?php
+define('DB_SERVER', 'localhost');
+define('DB_USERNAME', 'qsnlbwut_elemix');
+define('DB_PASSWORD', 'elemix');
+define('DB_NAME', 'qsnlbwut_elemix');
+ 
+/* Attempt to connect to MySQL database */
+$link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
+?>
