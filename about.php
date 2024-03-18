c0dbe307803bd1296f49ab3f4767359ae482b259
JoshCaramat <caramat.joshivan@gmail.com>
2024-03-18 21:58:50 +0800

    paragraph justified, padding added and margin-top

--- about.php
+++ about.php
@@ -14,20 +14,21 @@
     <script src="js/script.js"> </script>
     <title>EleMix</title>
 </head>
-<body class="p-0 m-0 position-relative d-flex flex-column ">
+<body class="p-0 m-0 position-relative d-flex flex-column">
     <section class="fixed-top position-relative">  
         <!-- <span class="position-absolute translate-middle-x">&#8592;</span>       -->
         <a class="position-absolute translate-middle-x" id="back_button" href="index.php" style="text-decoration:none;">🡠</a>      
         <img src="assets/image/elemix logo.png" alt="">
     </section>  
+    <div class="container">
 
-
-    <div class=" preface d-flex justify-content-center align-items-center" style="Margin: 10%; Font-family: Arial; ">
-    Lorem ipsum dolor sit amet, consectetur adipiscing elit. In eu mollis urna, nec varius felis. Quisque placerat efficitur laoreet. Nullam dignissim, odio vitae mollis venenatis, arcu arcu rutrum nibh, non lobortis tellus risus ut risus. Duis accumsan tempus purus sed suscipit. Etiam vel ante at risus semper sollicitudin non semper nisi. Quisque quis ante vitae leo lacinia viverra id et justo. Donec est turpis, pulvinar a imperdiet id, consequat non turpis. Praesent turpis metus, dictum at risus vel, feugiat pellentesque nulla.
-
-Morbi malesuada, quam at dictum efficitur, ipsum dui placerat dolor, ac posuere lacus libero in nisl. Fusce tristique tempor cursus. Praesent molestie leo eget lorem lobortis pulvinar vitae vitae nisl. Fusce ornare, mauris eget scelerisque molestie, massa velit viverra lectus, eget suscipit mi ex et libero. Donec non nibh risus. Curabitur a nunc eu nunc elementum aliquet. Nullam sit amet laoreet diam. Nunc feugiat libero et ipsum aliquam, a mollis dui consectetur. Aliquam ultrices fermentum efficitur. Praesent consequat tristique libero, at fringilla enim. Etiam sed efficitur diam. Etiam iaculis turpis elit, id tempus turpis pharetra in. Curabitur placerat nec diam a placerat.
-
-Pellentesque congue justo nec iaculis ornare. Cras dictum mauris tellus, at molestie felis mollis vitae. Nulla convallis neque tincidunt molestie vestibulum. Duis eget nunc in urna rhoncus mattis. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas ac libero venenatis justo tempus faucibus. Quisque tempus, lorem eget tempor dignissim, tellus enim imperdiet odio, eget pellentesque leo sapien ut dui. Morbi rutrum lacinia odio quis posuere. Sed tincidunt risus nec arcu maximus, nec rutrum tellus hendrerit. Phasellus efficitur, leo ut consectetur luctus, arcu lorem auctor nisl, sit amet hendrerit quam mi vitae magna. Praesent nec est posuere arcu suscipit lobortis. Fusce quis fermentum nibh. Nam non tristique nisl.
+        <p class="text-justify py-5 px-3 mt-1" style=" Font-family: Arial; text-align:justify; ">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. In eu mollis urna, nec varius felis. Quisque placerat efficitur laoreet. Nullam dignissim, odio vitae mollis venenatis, arcu arcu rutrum nibh, non lobortis tellus risus ut risus. Duis accumsan tempus purus sed suscipit. Etiam vel ante at risus semper sollicitudin non semper nisi. Quisque quis ante vitae leo lacinia viverra id et justo. Donec est turpis, pulvinar a imperdiet id, consequat non turpis. Praesent turpis metus, dictum at risus vel, feugiat pellentesque nulla.
+    
+        Morbi malesuada, quam at dictum efficitur, ipsum dui placerat dolor, ac posuere lacus libero in nisl. Fusce tristique tempor cursus. Praesent molestie leo eget lorem lobortis pulvinar vitae vitae nisl. Fusce ornare, mauris eget scelerisque molestie, massa velit viverra lectus, eget suscipit mi ex et libero. Donec non nibh risus. Curabitur a nunc eu nunc elementum aliquet. Nullam sit amet laoreet diam. Nunc feugiat libero et ipsum aliquam, a mollis dui consectetur. Aliquam ultrices fermentum efficitur. Praesent consequat tristique libero, at fringilla enim. Etiam sed efficitur diam. Etiam iaculis turpis elit, id tempus turpis pharetra in. Curabitur placerat nec diam a placerat.
+    
+        Pellentesque congue justo nec iaculis ornare. Cras dictum mauris tellus, at molestie felis mollis vitae. Nulla convallis neque tincidunt molestie vestibulum. Duis eget nunc in urna rhoncus mattis. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas ac libero venenatis justo tempus faucibus. Quisque tempus, lorem eget tempor dignissim, tellus enim imperdiet odio, eget pellentesque leo sapien ut dui. Morbi rutrum lacinia odio quis posuere. Sed tincidunt risus nec arcu maximus, nec rutrum tellus hendrerit. Phasellus efficitur, leo ut consectetur luctus, arcu lorem auctor nisl, sit amet hendrerit quam mi vitae magna. Praesent nec est posuere arcu suscipit lobortis. Fusce quis fermentum nibh. Nam non tristique nisl.
+        </p>
     </div>
 
 
