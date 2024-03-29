84f40932f00a9dbbfc67d8cc71a0f9b024365faf
Ivan Neil Tapel <tapsivanneil@gmail.com>
2024-03-29 20:18:04 +0800

    adjusted database sql for easier application

--- database.sql
+++ database.sql
@@ -1,67 +1,138 @@
-CREATE DATABASE elemix;
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Mar 29, 2024 at 01:15 PM
+-- Server version: 10.4.28-MariaDB
+-- PHP Version: 8.0.28
 
-USE elemix;
-
-CREATE TABLE combinations (
-    id INT(10) NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    element_1 VARCHAR(250),
-    subscript_1 INT(10),
-    element_2 VARCHAR(250),
-    subscript_2 INT(10),
-    combination VARCHAR(250),
-    combination_name VARCHAR(250),
-    source VARCHAR(250)
-) ;
-
-INSERT INTO combinations (element_1, subscript_1, element_2, subscript_2, combination, combination_name, source) VALUES 
-('N', 3, 'H', 1, 'NH3', 'Ammonia', 'assets/gif/NH3.gif'), 
-('Al', 3, 'Br', -1, 'AlBr3', 'Aluminum Bromide', 'assets/gif/AlBr3.gif')
-('Cd', 1, 'S', -1, 'CdS', 'Cadium Sulfide', 'assets/gif/CdS.gif'), 
-('H', 1, 'Cl', -1, 'HCl', 'Hydrochloric Acid', 'assets/gif/HCl.gif'), 
-('Na', 1, 'Cl', -1, 'NaCl', 'Sodium Chloride', 'assets/gif/NaCl.gif'), 
-('Ag', 1, 'Cl', -1, 'AgCl', 'Silver Chloride', 'assets/gif/AgCl.gif'), 
-('Hg', 2, 'O', -2, 'HgO', 'Mercuric Oxide', 'assets/gif/HgO.gif'), 
-('Pb', 2, 'I', 1, 'PbI2', 'Lead Iodide', 'assets/gif/PbI2.gif'), 
-('N', 2, 'Cl', -1, 'NiCl2', 'Nickel Chloride', 'assets/gif/NiCl2.gif'), 
-('P', 5, 'Br', -1, 'PBr5', 'Phosporus Pentabromide', 'assets/gif/PBr5.gif'); 
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
 
 
-CREATE TABLE elements (
-    id INT(10) NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    symbol VARCHAR(250),
-    element_name VARCHAR(250)
-);
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
 
+--
+-- Database: `elemix`
+--
 
-INSERT INTO elements (symbol, element_name)
-VALUES
-('N', 'Nitrogen'),
-('H', 'Hydrogen'),
-('Al', 'Aluminum'),
-('Br', 'Bromine'),
-('Cl', 'Chlorine'),
-('O', 'Oxygen'),
-('F', 'Fluorine'),
-('S', 'Sulfur'),
-('I', 'Iodine'),
-('Ba', 'Barium'),
-('Ca', 'Calcium'),
-('C', 'Carbon'),
-('Cd', 'Cadmium'),
-('P', 'Phosphorus'),
-('K', 'Potassium'),
-('Se', 'Selenium'),
-('Na', 'Sodium'),
-('Ag', 'Silver'),
-('Sr', 'Strontium'),
-('Zn', 'Zinc'),
-('Fe', 'Iron'),
-('As', 'Arsenic'),
-('Mg', 'Magnesium'),
-('Ni', 'Nickel'),
-('Ti', 'Titanium'),
-('Co', 'Cobalt'),
-('Hg', 'Mercury'),
-('Pb', 'Lead');
+-- --------------------------------------------------------
 
--- Add more elements if needed
+--
+-- Table structure for table `combinations`
+--
+
+CREATE TABLE `combinations` (
+  `id` int(10) NOT NULL,
+  `element_1` varchar(250) DEFAULT NULL,
+  `subscript_1` int(10) DEFAULT NULL,
+  `element_2` varchar(250) DEFAULT NULL,
+  `subscript_2` int(10) DEFAULT NULL,
+  `combination` varchar(250) DEFAULT NULL,
+  `combination_name` varchar(250) DEFAULT NULL,
+  `source` varchar(250) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `combinations`
+--
+
+INSERT INTO `combinations` (`id`, `element_1`, `subscript_1`, `element_2`, `subscript_2`, `combination`, `combination_name`, `source`) VALUES
+(1, 'Al', 3, 'Br', -1, 'AlBr3', 'Aluminum Bromide', 'assets/gif/AlBr3.gif'),
+(2, 'Cd', 1, 'S', -1, 'CdS', 'Cadium Sulfide', 'assets/gif/CdS.gif'),
+(3, 'H', 1, 'Cl', -1, 'HCl', 'Hydrochloric Acid', 'assets/gif/HCl.gif'),
+(4, 'Na', 1, 'Cl', -1, 'NaCl', 'Sodium Chloride', 'assets/gif/NaCl.gif'),
+(5, 'Ag', 1, 'Cl', -1, 'AgCl', 'Silver Chloride', 'assets/gif/AgCl.gif'),
+(6, 'Hg', 2, 'O', -2, 'HgO', 'Mercuric Oxide', 'assets/gif/HgO.gif'),
+(7, 'Pb', 2, 'I', 1, 'PbI2', 'Lead Iodide', 'assets/gif/PbI2.gif'),
+(8, 'N', 2, 'Cl', -1, 'NiCl2', 'Nickel Chloride', 'assets/gif/NiCl2.gif'),
+(9, 'P', 5, 'Br', -1, 'PBr5', 'Phosporus Pentabromide', 'assets/gif/PBr5.gif'),
+(10, 'Cd', 2, 'Se', -2, 'CdSe', 'Ammonia', 'assets/gif/CdSe.gif');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `elements`
+--
+
+CREATE TABLE `elements` (
+  `id` int(10) NOT NULL,
+  `symbol` varchar(250) DEFAULT NULL,
+  `element_name` varchar(250) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `elements`
+--
+
+INSERT INTO `elements` (`id`, `symbol`, `element_name`) VALUES
+(1, 'N', 'Nitrogen'),
+(2, 'H', 'Hydrogen'),
+(3, 'Al', 'Aluminum'),
+(4, 'Br', 'Bromine'),
+(5, 'Cl', 'Chlorine'),
+(6, 'O', 'Oxygen'),
+(7, 'F', 'Fluorine'),
+(8, 'S', 'Sulfur'),
+(9, 'I', 'Iodine'),
+(10, 'Ba', 'Barium'),
+(11, 'Ca', 'Calcium'),
+(12, 'C', 'Carbon'),
+(13, 'Cd', 'Cadmium'),
+(14, 'P', 'Phosphorus'),
+(15, 'K', 'Potassium'),
+(16, 'Se', 'Selenium'),
+(17, 'Na', 'Sodium'),
+(18, 'Ag', 'Silver'),
+(19, 'Sr', 'Strontium'),
+(20, 'Zn', 'Zinc'),
+(21, 'Fe', 'Iron'),
+(22, 'As', 'Arsenic'),
+(23, 'Mg', 'Magnesium'),
+(24, 'Ni', 'Nickel'),
+(25, 'Ti', 'Titanium'),
+(26, 'Co', 'Cobalt'),
+(27, 'Hg', 'Mercury'),
+(28, 'Pb', 'Lead');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `combinations`
+--
+ALTER TABLE `combinations`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `elements`
+--
+ALTER TABLE `elements`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `combinations`
+--
+ALTER TABLE `combinations`
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+
+--
+-- AUTO_INCREMENT for table `elements`
+--
+ALTER TABLE `elements`
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=29;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
