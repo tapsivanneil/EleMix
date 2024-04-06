81bb51406c92570bfaff21244e06878372a7c70b
Ivan Neil Tapel <tapsivanneil@gmail.com>
2024-04-07 06:59:34 +0800

    apply the changes with the process of mixing elements

--- elemix.php
+++ elemix.php
@@ -1,5 +1,5 @@
 <?php 
-    require_once "config/config_online.php";
+    require_once "config/config.php";
 ?>
 
 <!DOCTYPE html>
@@ -24,6 +24,12 @@
         <!-- <span class="position-absolute translate-middle-x">&#8592;</span>       -->
         <a class="position-absolute translate-middle-x" id="back_button" href="index.php" style="text-decoration:none;">🡠</a>      
         <img src="assets/image/elemix logo.png" alt="">
+
+        <button class="elemix-guide" type="button" data-bs-toggle="offcanvas" data-bs-target="#guideOffcanvas" aria-controls="offcanvasExample">
+        ?
+        </button>
+
+
     </section>  
     <div class="position-relative preface justify-content-center align-items-center">
         <div style="display: flex; flex-direction: column;">
@@ -41,26 +47,27 @@
                     <form method="post" class="d-flex flex-column justify-content-center position-relative">              
                         <div class="d-flex justify-content-around align-items-center" id="combination_holder">  
                             <div class="element-container position-relative justify-content-center">
-                                <button class="element_btn " onclick="setElementFocus(1)" name="element_1" id="element_1" data-bs-toggle="offcanvas" data-bs-target="#offcanvasExample" type="button" aria-controls="offcanvasExample">
+                                <button class="element_btn "  onclick="setElementFocus(1)" name="element_1" id="element_1" data-bs-toggle="offcanvas" data-bs-target="#offcanvasExample" type="button" aria-controls="offcanvasExample">
                                     Hg
                                 </button>
-                                <input oninput="getResult()" name="subscript_1" id="subscript_1" type="number" class="position-absolute top-100 end-0 translate-middle text-center border border-black border-1 rounded-circle" value="2">
+                                <div oninput="getResult()" name="subscript_1" id="subscript_1" type="number" class="superscript" >2</div>
                             </div>                
     
                             <div class="plus pb-3">+</div>
     
                             <div class="element-container position-relative">
-                                <button class="element_btn " onclick="setElementFocus(2)" name="element_2" id="element_2" data-bs-toggle="offcanvas" data-bs-target="#offcanvasExample" type="button" aria-controls="offcanvasExample">
+                                <button class="element_btn " onclick="setElementFocus(1)" name="element_2" id="element_2" data-bs-toggle="offcanvas" data-bs-target="#offcanvasExample" type="button" aria-controls="offcanvasExample">
                                     O
                                 </button>
-                                <input oninput="getResult()" name="subscript_2" id="subscript_2" type="number" class="position-absolute top-100 end-0 translate-middle text-center border border-black border-1 rounded-circle" value="-2">
+                                <div oninput="getResult()" name="subscript_2" id="subscript_2" type="number" class="superscript" >-2</div>
                             </div>  
                             <input type="hidden" name="element_result" id="element_result">
                             <input type="hidden" name="element_1_search" id="element_1_search" value="Hg">
                             <input type="hidden" name="element_2_search" id="element_2_search" value="O">
-    
-                        </div>           
-                                
+                            <input type="hidden" name="subscript_1_search" id="subscript_1_search" value="2">
+                            <input type="hidden" name="subscript_2_search" id="subscript_2_search" value="-2">
+
+                        </div>             
                         <input type="submit"  name="submit_combine" value="MIX" class="mix-btn mt-5 mx-auto">           
                     </form>
                 </div>
@@ -74,11 +81,6 @@
 
 <!-- offcanvas  -->
         <div class="offcanvas offcanvas-bottom" tabindex="-1" id="offcanvasExample" aria-labelledby="offcanvasExampleLabel">
-            <div class="offcanvas-header d-flex">
-                <form method="post" class="px-3 " style="width: 100%;">
-                    <input type="text" name="element" placeholder="Search element" class="rounded border-0 bg-secondary-subtle p-3 text-center" id="name_search" style="width: 100%;">                    
-                </form>                   
-            </div>
 
             <div class="offcanvas-body position-relative pt-3 mb-2" id="liveSearchResults">                 
 
@@ -87,10 +89,10 @@
                     if(isset($_POST['element_submit'])){
                         $element_1_search = isset($_POST['element_1_search']) ? $_POST['element_1_search'] : 'Na';
                         $element_2_search = isset($_POST['element_2_search']) ? $_POST['element_2_search'] : 'Cl';
-                        $subscript_1_search = isset($_POST['subscript_1']) ? $_POST['subscript_1'] : '1';
-                        $subscript_2_search = isset($_POST['subscript_2']) ? $_POST['subscript_2'] : '-1';
+                        $subscript_1_search = isset($_POST['subscript_1_search']) ? $_POST['subscript_1_search'] : '1';
+                        $subscript_2_search = isset($_POST['subscript_2_search']) ? $_POST['subscript_2_search'] : '-1';
 
-                        $stmt = $link->prepare("SELECT * FROM elements WHERE element_name LIKE ? OR symbol LIKE ? ");
+                        $stmt = $link->prepare("SELECT * FROM combinations WHERE element_name LIKE ? OR symbol LIKE ? ");
                         $likeParam = "%{$element_search}%";
                         $stmt->bind_param("ss", $likeParam, $likeParam );
                         $stmt->execute();
@@ -98,17 +100,22 @@
 
                     }
                     else{
-                        $stmt = $link->prepare("SELECT * FROM elements");
+                        $stmt = $link->prepare("SELECT * FROM combinations");
                         $stmt->execute();
                         $result = $stmt->get_result(); 
                     }
 
                     if($result->num_rows > 0){
                         while($element = $result->fetch_assoc()){
-                            $element_symbol = $element['symbol'];
-                            $element_name = $element['element_name'];
+                            $element_1 = $element['element_1'];
+                            $element_2 = $element['element_2'];
+                            $subscript_1 = $element['subscript_1'];
+                            $subscript_2 = $element['subscript_2'];
+                            $element_symbol = $element['combination'];
+                            $element_name = $element['combination_name'];
+
                             echo '
-                                <button onclick="setElementName(element_focus, \'' . $element_symbol . '\')"  getResult(); class="offcanvas-btn d-flex flex-column justify-content-center align-items-center mx-auto">
+                                <button onclick="setElementName( \'' . $element_1 . '\', \'' . $element_2 . '\',\'' . $subscript_1 . '\',\'' . $subscript_2 . '\')"  getResult(); class="offcanvas-btn d-flex flex-column justify-content-center align-items-center mx-auto">
                                     <span class="symbol">' . $element_symbol . '</span>
                                     <span class="element-name">' . $element_name . '</span>
                                 </button>';
@@ -119,32 +126,68 @@
 
             </div>
         </div>
-   <script>
-    $(document).ready(function () {
-        $('#name_search').on('input', function () {
-            var searchQuery = $(this).val();
 
-            $.ajax({
-                type: 'POST',
-                url: 'live_search.php',
-                data: { name_search: searchQuery },
-                success: function (data) {
-                    $('#liveSearchResults').html(data);
-                }
-            });
-        });
-    });
-</script>
+<div style="display: flex">
+    <button class="combination-btn" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasExample" aria-controls="offcanvasExample">
+  Combinations
+</button>
+
+</div>
+
+
+<div class="offcanvas offcanvas-start" tabindex="-1" id="guideOffcanvas" aria-labelledby="offcanvasExampleLabel">
+ 
+<div class="container">
+
+    <style>
+    
+    @font-face {
+    font-family: "Chewy";
+    src: url("assets/font/Chewy-Regular.woff") format("woff"),
+        url("assets/font/Chewy-Regular.woff2") format("woff2");
+    }
+    .image-container {
+        display: flex;
+        flex-wrap: wrap;
+        width: 100%;
+        height: 100vh;
+        overflow-x: auto;
+        margin: 0; /* Remove default margins */
+        padding: 0; /* Remove default padding */
+    }
+
+    .image-container img {
+        flex: 0 0 100%;
+        height: auto;
+    }
+
+
+    </style>
+  <div class="offcanvas-header" style="margin-top: 20px">
+    <h5 class="offcanvas-title" id="offcanvasLabel" style="font-family: Chewy; font-size: 200%; color: #e73813;">User Guide</h5>
+    <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+  </div>
+    <div class="image-container">
+        <img src="assets/image/1.png" alt="">
+        <img src="assets/image/2.png" alt="">
+        <img src="assets/image/3.png" alt="">
+        <img src="assets/image/4.png" alt="">
+        <img src="assets/image/5.png" alt="">
+
+    </div>
+
+
+
+</div>
 
 </body>
 </html>
 <?php
-        $search_combination = isset($_POST['element_result']) ? $_POST['element_result'] : "";
         $el_1 = isset($_POST['element_1_search']) ? $_POST['element_1_search'] : "";
         $el_2 = isset($_POST['element_2_search']) ? $_POST['element_2_search'] : "";
-        $sub_1 = isset($_POST['subscript_1']) ? $_POST['subscript_1'] : "";
-        $sub_2 = isset($_POST['subscript_2']) ? $_POST['subscript_2'] : "";
-        //echo '<script>alert("'.$el_1 .' '.$el_2.'");</script>';
+        $sub_1 = isset($_POST['subscript_1_search']) ? $_POST['subscript_1_search'] : "";
+        $sub_2 = isset($_POST['subscript_2_search']) ? $_POST['subscript_2_search'] : "";
+        echo '<script>console.log("'.$el_1 .' '.$el_2.''.$sub_1 .' '.$sub_2.'");</script>';
 
        
         $stmt = $link->prepare("SELECT source, combination 
--- js/script.js
+++ js/script.js
@@ -1,8 +1,21 @@
 let element_focus = 0;
 
-const setElementName = (input_name, element_name) => {
-  var element = document.getElementById(input_name);
-  element.innerHTML = element_name;
+const setElementName = (
+  element_1_val,
+  element_2_val,
+  subscript_1_val,
+  subscript_2_val
+) => {
+  var element_1 = document.getElementById("element_1");
+  var element_2 = document.getElementById("element_2");
+  var subscript_1 = document.getElementById("subscript_1");
+  var subscript_2 = document.getElementById("subscript_2");
+
+  element_1.innerHTML = element_1_val;
+  element_2.innerHTML = element_2_val;
+  subscript_1.innerHTML = subscript_1_val;
+  subscript_2.innerHTML = subscript_2_val;
+
   getResult();
 };
 
@@ -12,19 +25,17 @@ const setElementFocus = (num) => {
 
 const getResult = () => {
   //console.log("lupet");
-  var el_1 = document.getElementById("element_1").innerText;
-  var el_2 = document.getElementById("element_2").innerText;
-  var el_1_search = document.getElementById("element_1_search");
-  var el_2_search = document.getElementById("element_2_search");
-  var sub_1 = document.getElementById("subscript_1").value;
-  var sub_2 = document.getElementById("subscript_2").value;
+  var el_1 = document.getElementById("element_1").innerHTML;
+  var el_2 = document.getElementById("element_2").innerHTML;
 
-  el_1 = document.getElementById("element_1").innerHTML;
-  el_2 = document.getElementById("element_2").innerHTML;
+  var sub_1 = document.getElementById("subscript_1").innerHTML;
+  var sub_2 = document.getElementById("subscript_2").innerHTML;
 
-  el_1_search.value = el_1;
-  el_2_search.value = el_2;
+  var el_1 = (document.getElementById("element_1_search").value = el_1);
+  var el_2 = (document.getElementById("element_2_search").value = el_2);
 
-  var element_res = document.getElementById("element_result");
-  element_res.value = el_1 + sub_1 + el_2 + sub_2;
+  var sub_1 = (document.getElementById("subscript_1_search").value = sub_1);
+  var sub_2 = (document.getElementById("subscript_2_search").value = sub_2);
+
+  // console.log(el_1 + sub_1 + el_2 + sub_2);
 };
